55c5bbec52b2821cd5523b7cfff00c26bd29087e
subashini13 <112946397+subashini13@users.noreply.github.com>
2026-04-15 09:54:48 +0100

    Refactor TicketPriceLoader for better error handling
    Signed-off-by: subashini13 <112946397+subashini13@users.noreply.github.com>

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketPriceLoader.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketPriceLoader.java
@@ -1,5 +1,7 @@
 package uk.gov.dwp.uc.pairtest;
 
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
 import java.io.IOException;
 import java.util.Properties;
 
@@ -8,13 +10,21 @@ public class TicketPriceLoader {
     private static final Properties properties = new Properties();
 
     static {
-        try {
+        try{
             properties.load(TicketPriceLoader.class.getClassLoader().getResourceAsStream("ticket-prices.properties"));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load ticket prices", e);
+
+        }catch(IOException e){
+            throw new RuntimeException("Ticket price file cannot be loaded.",e);
         }
     }
-    public static int getPrice(String ticketType) {
-        return Integer.parseInt(properties.getProperty(ticketType));
+
+    public static int getPrice(String ticketType){
+        String price = properties.getProperty(ticketType);
+        return Integer.parseInt(price);
+    }
+
+    public void setMockProperties(Properties mockProperties){
+        properties.clear();
+        properties.putAll(mockProperties);
     }
 }
